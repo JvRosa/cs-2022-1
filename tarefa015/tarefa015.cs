09a38e610c402041f4354c2dc90c39f285f132e8
João Victor Rosa Couto e Silva <jvrosacoutosilva@gmail.com>
2022-08-12 23:39:01 -0300

    Create tarefa015.cs

---
+++ tarefa015/tarefa015.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ExSenha
+{
+public static class Program
+{
+static void Main(string[] args)
+{
+Regex reLogin = new("^(?=.*[A-z])(?=.*[0-9])(?=.*[@!#$%^&*.~()\\])[@!#$%^&*.~()\\a-zA-Z0-9]{5,30}$", RegexOptions.Compiled);
+Regex rePassw = new("^(?=.*[A-Z])(?=.*[0-9])(?=.*[@!#$%^&*.~()\\])[@!#$%^&*.~()\\a-zA-Z0-9]{8,12}$", RegexOptions.Compiled);
+
+string? login = Console.ReadLine();
+string? senha = Console.ReadLine();
+}
+}
+}
